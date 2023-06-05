2cffd2a25a703c75d6a18135d1c2093b189f826d
Manny Aguilar <38090225+Manny-Code-Bytes@users.noreply.github.com>
2023-06-04 19:55:27 -0600

    Used getColor function to retrieve and assign colors from CSS variables

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-// function to get custom css variables as colors to use for gradient
+// function to get custom css variables as color to use for gradient
 const getColor = (color) => {
     return getComputedStyle(document.documentElement).getPropertyValue(color);
 }
+// get all colors from css file
+// can add or remove colors to use 
+const color1 = getColor('--c1');
+const color2 = getColor('--c2');
+const color3 = getColor('--c3');
+const color4 = getColor('--c4');
+const color5 = getColor('--c5');
+const color6 = getColor('--c6');
+const color7 = getColor('--c7');
+const color8 = getColor('--c8');
+const color9 = getColor('--c9');
